9ed612e04d2a93cb2a31ca1c3d360b39ac9d8915
Jannis Schaefer <j.schaefer@estwx.de>
2018-03-09 20:43:01 +0100

    add unittest for simplecropper

---
+++ processing/crop/simplecropper_test.go
@@ -0,0 +1,70 @@
+package crop
+
+import (
+	"github.com/stretchr/testify/assert"
+	"image"
+	"os"
+	"testing"
+)
+
+func TestSimpleCropperImageSize(t *testing.T) {
+	// Setup
+	file, err := os.Open("./../../assets/snow-1920.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cropper := SimpleCropper{}
+
+	// Test
+	result, err := cropper.Crop(img, 400, 300)
+
+	// Assertions
+	// For more straightforward (JUnit like) assertions for example use github.com/stretchr/testify
+	if err != nil {
+		t.Fatalf("Error executing Crop: %+v", err)
+	}
+
+	width := result.Bounds().Max.X - result.Bounds().Min.X
+	if width != 300 {
+		t.Fatalf("Image width should be 300 but was %v", width)
+	}
+
+	height := result.Bounds().Max.Y - result.Bounds().Min.Y
+	if height != 400 {
+		t.Fatalf("Image width should be 400 but was %v", height)
+	}
+}
+
+func TestSimpleCropperImageSizeTestify(t *testing.T) {
+	// Setup
+	file, err := os.Open("./../../assets/snow-1920.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cropper := SimpleCropper{}
+
+	// Test
+	result, err := cropper.Crop(img, 400, 300)
+
+	// Assertions
+	assert.NoError(t, err)
+
+	width := result.Bounds().Max.X - result.Bounds().Min.X
+	height := result.Bounds().Max.Y - result.Bounds().Min.Y
+	assert.Equal(t, 300, width)
+	assert.Equal(t, 400, height)
+}
